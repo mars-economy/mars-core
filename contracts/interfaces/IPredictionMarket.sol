5bda322bed8ea72f8926966aa7686bab865e1cf5
fgrey650 <fgrey650@gmail.com>
2021-05-04 15:21:06 +0300

    Fixed prediction dates

--- contracts/interfaces/IPredictionMarket.sol
+++ contracts/interfaces/IPredictionMarket.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
+import "../libraries/Market.sol";
+
+
 interface IPredictionMarket {
     event PredictionEvent(address indexed _account, bytes16 _outcome, uint256 _amount);
 
@@ -19,6 +22,8 @@ interface IPredictionMarket {
     function predict(bytes16 _outcome, uint256 _amount) external;
 
     function getTokens() external view returns (address[] memory);
+
+	function getUserPredictionState() external view returns (Market.UserOutcomeInfo[] memory);
 
     function getReward() external;
 
